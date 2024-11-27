7ddff07240ca3f75d8ffa01cba7ec420efc0e00d
Aaaaron <ischenwl@gmail.com>
2024-11-27 16:12:19 +0800

    怎麼處理好了哈哈哈

--- app/src/main/java/com/example/tip102_group2/Peng.kt
+++ app/src/main/java/com/example/tip102_group2/Peng.kt
@@ -1,11 +1,12 @@
 package com.example.tip102_group2
 
+fun main() {
+    println("甚麼！處理好了嗎！發生甚麼事！")
+    println("$name")
+}
+
 fun Mcdonald(){
     hahahahah
 }
 println("嗨嗨")
 val name: String = "PENG"
-
-fun main() {
-    println("$name")
-}
